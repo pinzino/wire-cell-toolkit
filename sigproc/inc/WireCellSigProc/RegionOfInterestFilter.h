9417f73256cdf1797777162bcd06f36fec9273c8
jacopo pinzino <jacopo.pinzino@cern.ch>
2020-05-02 02:51:26 +0200

    added RegionOfInterestFilter

---
+++ sigproc/inc/WireCellSigProc/RegionOfInterestFilter.h
@@ -0,0 +1,76 @@
+/** Remove all possible noise from a microboone-like detector.
+ *
+ * This filter is a kitchen sink class and is a candidate for
+ * factoring.
+ */
+
+#ifndef WIRECELLSIGPROC_REGIONOFINTERESTFILTER
+#define WIRECELLSIGPROC_REGIONOFINTERESTFILTER
+
+#include "WireCellIface/IFrameFilter.h"
+#include "WireCellIface/IConfigurable.h"
+#include "WireCellIface/IChannelNoiseDatabase.h"
+#include "WireCellIface/IChannelFilter.h"
+
+#include "WireCellUtil/Waveform.h"
+#include "WireCellUtil/Logging.h"
+
+
+#include <vector>
+#include <map>
+#include <string>
+
+namespace WireCell {
+    namespace SigProc {
+
+	/**
+	   The Omnibus Noise Filter applies two series of IChannelFilter
+	   objects. The first series is applied on a per-channel basis and
+	   the second is applied on groups of channels as determined by
+	   its channel grouping.
+	*/
+	class RegionOfInterestFilter : public WireCell::IFrameFilter, public WireCell::IConfigurable {
+	public:
+	    typedef std::vector< std::vector<int> > grouped_channels_t;
+
+	    /// Create an RegionOfInterestFilter.
+	    RegionOfInterestFilter(const std::string& roi_tag = "roi");
+	    virtual ~RegionOfInterestFilter();
+
+	    /// IFrameFilter interface.
+	    virtual bool operator()(const input_pointer& in, output_pointer& out);
+
+	    /// IConfigurable interface.
+	    virtual void configure(const WireCell::Configuration& config);
+	    virtual WireCell::Configuration default_configuration() const;
+
+
+
+	private:
+
+      struct OspChan {
+        int channel;            // between 0 and nwire_u+nwire_v+nwire_w-1
+        int wire;               // between 0 and nwire_{u,v,w,}-1 depending on plane
+        int plane;              // 0,1,2
+        int ident;              // wct ident, opaque non-negative number.  set in wires geom file
+        OspChan(int c=-1, int w=-1, int p=-1, int id=-1) : channel(c), wire(w), plane(p), ident(id) {}
+        std::string str() const;
+      };
+      
+		std::string m_roi_tag;
+		std::string m_frame_tag;
+        Log::logptr_t log;
+		Waveform::ChannelMaskMap m_cmm; 
+
+		// Need to go from WCT channel ident to {OSP channel, wire and plane}
+		std::map<int,OspChan> m_channel_map;
+	};
+
+    }
+}
+#endif
+
+// Local Variables:
+// mode: c++
+// c-basic-offset: 4
+// End:
---
+++ sigproc/src/RegionOfInterestFilter.cxx
@@ -0,0 +1,158 @@
+#include "WireCellSigProc/RegionOfInterestFilter.h"
+
+#include "WireCellSigProc/Diagnostics.h"
+
+#include "WireCellUtil/Response.h"
+
+#include "WireCellIface/SimpleFrame.h"
+#include "WireCellIface/SimpleTrace.h"
+
+#include "WireCellUtil/NamedFactory.h"
+// #include "WireCellUtil/ExecMon.h" // debugging
+
+#include "FrameUtils.h"          // fixme: needs to move to somewhere more useful.
+
+#include <unordered_map>
+
+WIRECELL_FACTORY(RegionOfInterestFilter, WireCell::SigProc::RegionOfInterestFilter,
+                 WireCell::IFrameFilter, WireCell::IConfigurable)
+
+using namespace WireCell;
+
+using namespace WireCell::SigProc;
+
+RegionOfInterestFilter::RegionOfInterestFilter(const std::string& roi_tag)
+: m_roi_tag(roi_tag)
+, m_frame_tag("sigproc")
+, log(Log::logger("sigproc"))
+{
+}
+RegionOfInterestFilter::~RegionOfInterestFilter()
+{
+}
+
+void RegionOfInterestFilter::configure(const WireCell::Configuration& cfg)
+{
+  m_roi_tag = get(cfg,"roi_tag",m_roi_tag);   
+  m_frame_tag = get(cfg,"frame_tag",m_frame_tag); 
+}
+
+WireCell::Configuration RegionOfInterestFilter::default_configuration() const
+{
+    Configuration cfg;    
+    cfg["roi_tag"] = m_roi_tag;
+    cfg["frame_tag"] = m_frame_tag;
+    return cfg;
+}
+
+
+bool RegionOfInterestFilter::operator()(const input_pointer& inframe, output_pointer& outframe)
+{
+
+    log->debug("RegionOfInterestFilter: inside operator");
+
+    if (!inframe) {             // eos
+        outframe = nullptr;
+        return true;
+    }
+    // Convert to OSP cmm indexed by OSB sequential channels, NOT WCT channel ID.
+    m_cmm.clear();
+    for (auto cm : inframe->masks())
+    {  //in->masks restituisce a map (ChannelMaskMap) fatta da una stringa (che va in name) e un altra map (in m) ChannelMasks
+        const std::string name = cm.first;
+        for (auto m: cm.second)
+        {   //m è la mappa ChannelMasks fatta da un int (wct_channel_ident) e da un BinRangeList
+            const int wct_channel_ident = m.first;
+            const OspChan& och = m_channel_map[wct_channel_ident]; //viene dalla configurazione
+            if (och.plane < 0) continue;               // in case user gives us multi apa frame
+            m_cmm[name][och.channel] = m.second;  //m.second è la BinRangeList
+        }
+    }
+
+    IFrame::trace_list_t roi_traces;
+    ITrace::vector* newtraces = new ITrace::vector; // will become shared_ptr.
+
+    // outframe = inframe;
+
+    auto traces = inframe->traces();
+
+
+    for (auto trace : *traces.get())
+    {
+        int channel = trace->channel();
+        int tbin = trace->tbin();
+        auto const& charges = trace->charge();
+
+        ITrace::ChargeSequence newcharge(charges.size(), 0.0);
+
+        auto chargessort = charges;
+
+        std::nth_element(chargessort.begin(), chargessort.begin() + chargessort.size()/2, chargessort.end());
+        float median = chargessort[chargessort.size()/2];
+
+        log->debug("RegionOfInterestFilter: canale {}, tempo iniziale {}", channel, tbin);  
+
+        for (int bin = 0; bin < (int)charges.size(); ++bin)
+        {
+           //  float left_tail = 0.0;
+           //  float right_tail = 0.0;
+           //  float tail = 0.0;
+           //  float central_value = 0.0;
+           //  int ntail = 0;
+           //  if (bin>49)
+           //  {
+           //      for (int i = 0; i < 20; ++i)
+           //      {
+           //          left_tail += charges[bin-49+i]/20.; 
+           //      }
+           //      tail += left_tail;
+           //      ntail++;
+           //  }   
+           //  if (bin < ((int)charges.size()-49))
+           //  {              
+           //      for (int i = 0; i < 20; ++i)
+           //      {
+           //          right_tail += charges[bin+49-i]/20.; 
+           //      }
+           //      tail += right_tail;
+           //      ntail++;
+           // } 
+           // tail = tail/ntail;
+           // central_value = charges[bin]- tail;
+            // log->debug("RegionOfInterestFilter: carica nel bin {} = {}, tail({}. {}, {}), Cvalue {}, median {}", bin, charges[bin], left_tail, right_tail, tail, central_value, median );  
+
+            float central_value = charges[bin]- median;
+            log->debug("RegionOfInterestFilter: carica nel bin {} = {}, median {}, Cvalue {}", bin, charges[bin], median, central_value );  
+
+            newcharge.at(bin) = central_value;
+
+        }
+
+        SimpleTrace *tracetemp = new SimpleTrace(channel, tbin, newcharge);
+        const size_t trace_index = newtraces->size();
+        roi_traces.push_back(trace_index);
+        newtraces->push_back(ITrace::pointer(tracetemp));
+
+    }
+
+
+
+    SimpleFrame* sframe = new SimpleFrame(inframe->ident(), inframe->time(),
+                                        ITrace::shared_vector(newtraces),
+                                        inframe->tick(), m_cmm);
+    sframe->tag_frame(m_frame_tag);
+    sframe->tag_traces(m_roi_tag, roi_traces);
+
+    outframe = IFrame::pointer(sframe);
+
+    // outframe = IFrame::pointer(sframe);
+    log->debug("RegionOfInterestFilter: end operator");
+
+    return true;
+}
+
+
+// Local Variables:
+// mode: c++
+// c-basic-offset: 4
+// End:
